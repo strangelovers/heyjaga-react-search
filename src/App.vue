28663dbbe9baf1a12d337569d6b1d80c7aface91
strangelovers <110255159+strangelovers@users.noreply.github.com>
2022-12-24 17:06:28 +0000

    dev deps

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,34 @@
 <template>
   <v-app>
-    <Navb
+    <Navbar />
+    <v-content>
+      <router-view></router-view>
+    </v-content>
+    <MyFooter />
+  </v-app>
+</template>
+
+<script>
+import Navbar from "@/components/Navbar";
+import MyFooter from "@/components/MyFooter";
+
+export default {
+  name: "App",
+  components: { Navbar, MyFooter },
+  data: () => ({})
+};
+</script>
+
+<style>
+#app {
+  color: white;
+  text-align: center;
+  background: url("./assets/newimage2.jpg") fixed;
+  background-color: #cccccc;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-container>
+    <v-layout
+      text-center
+      wrap
+    >
+      <v-flex xs12>
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
