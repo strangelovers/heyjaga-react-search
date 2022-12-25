58eed8a5cbbd8bb345bc5edf29a081c97702085c
strangelovers <110255159+strangelovers@users.noreply.github.com>
2022-12-25 06:40:57 +0000

    Bump package.json version

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,3 +10,33 @@
           class="my-3"
           contain
           height="200"
+        ></v-img>
+      </v-flex>
+
+      <v-flex mb-4>
+        <h1 class="display-2 font-weight-bold mb-3">
+          Welcome to Vuetify
+        </h1>
+        <p class="subheading font-weight-regular">
+          For help and collaboration with other Vuetify developers,
+          <br>please join our online
+          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
+        </p>
+      </v-flex>
+
+      <v-flex
+        mb-5
+        xs12
+      >
+        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+
+        <v-layout justify-center>
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+        
