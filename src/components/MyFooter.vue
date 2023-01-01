140d7db4d504a1fbdfeac38f917af8475f158557
strangelovers <110255159+strangelovers@users.noreply.github.com>
2023-01-01 23:07:22 +0000

    Events Update

--- src/components/MyFooter.vue
+++ src/components/MyFooter.vue
@@ -1,4 +1,22 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>© 2019 API provided 
+    <p>© 2019 API provided by CoinMarketCap</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyFooter",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p {
+  margin-top: 30px;
+}
+</style>
---
+++ src/components/MyTable.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="font-weight-medium mytable">
+    <v-data-table
+      :headers="headers"
+      @click:row="getCrypto"
+      :items="fixedDecimals"
+      :items-per-page="5"
+      :mobile-breakpoint="500"
+      class="elevation-1"
+    ></v-data-table>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import { loadavg } from "os";
+
+export default {
+  name: "MyTable",
+  data() {
+    return {
+      headers: [
+        { text: "Rank", value: "cmc_rank" },
+        {
+          text: "Name",
+      
