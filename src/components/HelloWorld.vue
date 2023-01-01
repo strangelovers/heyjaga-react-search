b5a2c40ec747e9049d5e7c4cb14e64c3a897eaf4
strangelovers <110255159+strangelovers@users.noreply.github.com>
2023-01-01 19:21:56 +0000

    updated api

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -112,4 +112,31 @@ export default {
       },
       {
         text: 'Made with Vuetify',
-        href: 'https
+        href: 'https://madewithvuejs.com/vuetify',
+      },
+      {
+        text: 'Twitter',
+        href: 'https://twitter.com/vuetifyjs',
+      },
+      {
+        text: 'Articles',
+        href: 'https://medium.com/vuetify',
+      },
+    ],
+    whatsNext: [
+      {
+        text: 'Explore components',
+        href: 'https://vuetifyjs.com/components/api-explorer',
+      },
+      {
+        text: 'Select a layout',
+        href: 'https://vuetifyjs.com/layout/pre-defined',
+      },
+      {
+        text: 'Frequently Asked Questions',
+        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
+      },
+    ],
+  }),
+};
+</script>
---
+++ src/components/MyFooter.vue
@@ -0,0 +1,4 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>© 2019 API provided 
