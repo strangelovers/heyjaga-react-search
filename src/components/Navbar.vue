924597ddd24d9114ac5b344cd23375de307c66ae
strangelovers <110255159+strangelovers@users.noreply.github.com>
2023-01-04 16:20:06 +0000

    Better formatting

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,4 +4,24 @@
       <v-toolbar-title>
         <router-link :to="titlelink.route">
           <div>
-            <!--<img class="logo" src="../assets/cr
+            <!--<img class="logo" src="../assets/cryptologo.png" alt="cryptologo" srcset />-->
+            <h4>{{ titlelink.text}}</h4>
+          </div>
+        </router-link>
+      </v-toolbar-title>
+
+      <div class="flex-grow-1"></div>
+      <template>
+        <v-toolbar-items class="hidden-xs-only" v-for="(link, index) in links" :key="index">
+          <v-btn :to="link.route" text>{{link.text}}</v-btn>
+        </v-toolbar-items>
+        <v-toolbar-items class="hidden-sm-and-up" v-for="link in links" :key="link.text">
+          <v-btn :to="link.route" icon>
+            <v-icon>{{ link.icon}}</v-icon>
+          </v-btn>
+        </v-toolbar-items>
+      </template>
+      <div class="flex-grow-1"></div>
+      <template>
+        <router-link v-if="logged" :to="loginlink.route" tag="v-btn">
+   
