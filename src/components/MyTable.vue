9cb780f46ee818b6c1cf9b11d390aa0633b1ae77
strangelovers <110255159+strangelovers@users.noreply.github.com>
2023-01-04 05:37:22 +0000

    sp

--- src/components/MyTable.vue
+++ src/components/MyTable.vue
@@ -23,4 +23,29 @@ export default {
         { text: "Rank", value: "cmc_rank" },
         {
           text: "Name",
-      
+          sortable: false,
+          value: "name"
+        },
+        { text: "Symbol", value: "symbol", sortable: false },
+        { text: "Price", value: "quote[USD][price]" },
+        { text: "% Change", value: "quote[USD][percent_change_24h]" },
+
+        { text: "Market Cap", value: "quote[USD][market_cap]", sortable: false }
+      ]
+    };
+  },
+  methods: {
+    getCrypto(item) {
+      console.log(item.id);
+      this.$router.replace("/cryptoitem/" + item.id);
+    }
+  },
+  computed: {
+    ...mapGetters(["allCryptos", "getSearch"]),
+    fixedDecimals() {
+      let cryptos = this.allCryptos.filter(coin =>
+        coin.name.toLowerCase().includes(this.getSearch.toLowerCase())
+      );
+
+      let newData = Array.from(cryptos);
+      for (var i = 0; i < cryptos
