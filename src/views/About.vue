b3f2fc72277b5f9149af80e574b033da0172f95e
strangelovers <110255159+strangelovers@users.noreply.github.com>
2023-01-15 17:36:50 +0000

    Release v1.4.2

--- src/views/About.vue
+++ src/views/About.vue
@@ -7,4 +7,14 @@
       transfer of assets. Cryptocurrencies use decentralized control as opposed to centralized digital currency and central
       banking systems. The decentralized control of each cryptocurrency works through distributed ledger technology, typically
       a blockchain, that serves as a public financial transaction database.
-      Bitcoin, first released as open-source software in 2009, i
+      Bitcoin, first released as open-source software in 2009, is generally considered the first decentralized cryptocurrency.
+      Since the release of bitcoin, over 4,000 altcoins (alternative variants of bitcoin, or other cryptocurrencies) have been created.
+    </p>
+
+    <div class="button" @click="readMore = !readMore" v-show="!readMore">Read More...</div>
+    <template v-if="readMore">
+      <h2 class="font-italic font-weight-medium">Arcitecture</h2>
+      <p>
+        Decentralized cryptocurrency is produced by the entire cryptocurrency system collectively, at a rate which is defined
+        when the system is created and which is publicly known. In centralized banking and economic systems such as the Federal Reserve
+        System, corporate boards or governments control the supply 
