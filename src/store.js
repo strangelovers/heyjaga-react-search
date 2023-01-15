22b8dcdc2759afc11392704fa78348d4644fa487
strangelovers <110255159+strangelovers@users.noreply.github.com>
2023-01-15 11:12:28 +0000

    add health check

--- src/store.js
+++ src/store.js
@@ -58,3 +58,52 @@ const actions = {
   },
   getSingleCrypto({ commit }, id) {
     commit("setLoading", true);
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    axios
+      .get(
+        proxyurl +
+          "https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?id=" +
+          id,
+        {
+          headers: {
+            "X-CMC_PRO_API_KEY": "e1eb1f30-5c4e-43aa-be04-4b50df00807a"
+          }
+        }
+      )
+      .then(response => {
+        console.log(response);
+        commit("setSingleCrypto", response.data.data);
+        commit("setLoading", false);
+      })
+      .catch(function(err) {
+        console.log(err);
+        commit("setLoading", false);
+      });
+  },
+  searchedCrypto({ commit }, payload) {
+    console.log();
+
+    commit("search", payload);
+  },
+  setMessages({ commit }, payload) {
+    commit("setMessages", payload);
+  }
+};
+const mutations = {
+  setCryptos: (state, payload) => (state.cryptodata = payload),
+  setSingleCrypto: (state, payload) => (state.singleCrypto = payload),
+  search: (state, payload) => (state.search = payload),
+  setLoading: (state, payload) => (state.loading = payload),
+  logged: (state, payload) => (state.logged = payload),
+  setMessages: (state, payload) => (state.chat = payload)
+};
+
+export default new Vuex.Store({
+  state,
+
+  actions,
+
+  getters,
+
+  mutations
+});
---
+++ src/views/About.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="about">
+    <h2 class="font-italic font-weight-medium">The History of Cryptocurrency</h2>
+    <p>
+      A cryptocurrency (or crypto currency) is a digital asset designed to work as a medium of exchange that
+      uses strong cryptography to secure financial transactions, control the creation of additional units, and verify the
+      transfer of assets. Cryptocurrencies use decentralized control as opposed to centralized digital currency and central
+      banking systems. The decentralized control of each cryptocurrency works through distributed ledger technology, typically
+      a blockchain, that serves as a public financial transaction database.
+      Bitcoin, first released as open-source software in 2009, i
