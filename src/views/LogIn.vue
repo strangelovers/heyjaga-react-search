9747f037bd10faad259b0e5ef6045d00966d1e91
strangelovers <110255159+strangelovers@users.noreply.github.com>
2023-02-25 14:50:08 +0000

    Add support for Python 3.9

--- src/views/LogIn.vue
+++ src/views/LogIn.vue
@@ -278,4 +278,26 @@ p.text {
   visibility: visible;
   width: 80px;
   height: 80px;
-  border: 2px solid #f3f3f
+  border: 2px solid #f3f3f3;
+  border-top: 3px solid #cffcfc;
+  border-radius: 100%;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: spin 1s infinite linear;
+}
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+.col {
+  padding: 0px !important;
+}
+</style>
---
+++ src/views/MyChart.vue
@@ -0,0 +1,22 @@
+
+
+<script>
+import { Bar } from "vue-chartjs";
+import { mapGetters } from "vuex";
+
+export default {
+  extends: Bar,
+  props: ["id", "cryptoFromTable"],
+
+  computed: {
+    ...mapGetters(["singleCrypto"]),
+    name() {
+      return this.singleCrypto[this.id].name;
+    },
+    priceNow() {
+      return this.cryptoFromTable.quote.USD.price;
+    },
+    price1h() {
+      return (
+        this.priceNow -
+        this.priceNow * (this.cryptoFromTable.quote.USD.
