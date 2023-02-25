63bd224893a3c590d7e2f1a79f60e3ebab250f20
strangelovers <110255159+strangelovers@users.noreply.github.com>
2023-02-25 21:45:49 +0000

    add .gitattributes file

--- src/views/MyChart.vue
+++ src/views/MyChart.vue
@@ -19,4 +19,41 @@ export default {
     price1h() {
       return (
         this.priceNow -
-        this.priceNow * (this.cryptoFromTable.quote.USD.
+        this.priceNow * (this.cryptoFromTable.quote.USD.percent_change_1h / 100)
+      );
+    },
+    price24h() {
+      return (
+        this.priceNow -
+        this.priceNow *
+          (this.cryptoFromTable.quote.USD.percent_change_24h / 100)
+      );
+    },
+    price7d() {
+      return (
+        this.priceNow -
+        this.priceNow * (this.cryptoFromTable.quote.USD.percent_change_7d / 100)
+      );
+    }
+  },
+
+  mounted() {
+    this.renderChart(
+      {
+        labels: ["7 days", "24h", "1h", "current"],
+        datasets: [
+          {
+            label: this.name,
+            backgroundColor: "#cffcfc",
+            data: [this.price7d, this.price24h, this.price1h, this.priceNow]
+          }
+        ]
+      },
+      { responsive: true, maintainAspectRatio: false }
+    );
+  }
+};
+</script>
+
+<style>
+</style>
